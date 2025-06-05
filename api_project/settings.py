d3912930e0249b96d6c62e86a5d7f9b96809c1d9
{shimosako-mikiko} <ys.training240714@gmail.com>
2025-06-05 18:39:03 +0900

    ログイン機能実装

--- api_project/settings.py
+++ api_project/settings.py
@@ -125,5 +125,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'  # ← collectstatic の出力先
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
--- api_project/urls.py
+++ api_project/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from books.views import BookViewSet  # ← BookViewSet をインポート
 
 router = routers.DefaultRouter()
@@ -25,4 +29,6 @@ router.register(r'books', BookViewSet)  # ← ルーターに登録
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),  # ← API のパスを追加
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
