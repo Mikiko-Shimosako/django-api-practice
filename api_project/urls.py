8b799f3d6db32dfbed6d731bacd31307e6d79130
{shimosako-mikiko} <ys.training240714@gmail.com>
2025-06-05 16:49:12 +0900

    データの登録

--- api_project/urls.py
+++ api_project/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from books.views import BookViewSet  # ← BookViewSet をインポート
+
+router = routers.DefaultRouter()
+router.register(r'books', BookViewSet)  # ← ルーターに登録
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),  # ← API のパスを追加
 ]
