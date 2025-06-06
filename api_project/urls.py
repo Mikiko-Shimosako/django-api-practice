ff6f9c86ac7c6527c9e5829c9e3b2927343c95a6
{shimosako-mikiko} <ys.training240714@gmail.com>
2025-06-06 10:55:04 +0900

    ユーザー作成準備

--- api_project/urls.py
+++ api_project/urls.py
@@ -31,6 +31,4 @@ urlpatterns = [
     path('', include(router.urls)),  # ← API のパスを追加
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    path('api/', include('accounts.urls')),  # ← ユーザー登録APIへのルーティング
 ]
